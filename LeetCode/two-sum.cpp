e33a73c4917b37abca6ca2a123adb50090f7641a
Snehil <snehilwebber@gmail.com>
2021-05-30 11:41:42 +0530

    Day 11 - Q1 (One-pass)

--- LeetCode/two-sum.cpp
+++ LeetCode/two-sum.cpp
@@ -1,24 +1,18 @@
 // LEETCODE PROBLEM - https://leetcode.com/problems/two-sum/
 
-class Solution
-{
-public:
-  vector<int> twoSum(vector<int> &nums, int target)
-  {
-    vector<int> result;
-    for (int i = 0; i < nums.size() - 1; i++)
-    {
-      int diff = target - nums[i];
-      for (int j = i + 1; j < nums.size(); j++)
-      {
-        if (nums[j] == diff)
-        {
-          result.push_back(i);
-          result.push_back(j);
-          return result;
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> lookup;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            if (lookup.find(target - nums[i]) != lookup.end()) {
+                res.push_back(i);
+                res.push_back(lookup[target - nums[i]]);
+                break;
+            }
+            lookup[nums[i]] = i;
         }
-      }
+        return res;
     }
-    return result;
-  }
 };
